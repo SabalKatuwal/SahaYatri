86e7a00a99334d3df695b0f4abd7db20fc80f2fc
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-09-29 16:11:19 +0545

    Initial Commit

---
+++ SahaYatri/SahaYatriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahaYatriApp.swift
+//  SahaYatri
+//
+//  Created by Sabal on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SahaYatriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
